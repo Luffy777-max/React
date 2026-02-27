408d81f3da2daef9eb9351eb6717ffadc395ecdc
Luffy <lokeshagarwal7700@gmail.com>
2026-02-27 18:31:43 +0530

    update

--- 05passwordgen/src/App.jsx
+++ 05passwordgen/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState,useCallback,useEffect,useRef } from 'react'
 import './App.css'
-
+//learned hooks - useState, useEffect, useRef, useCallback
 
 function App() {
   const [length, setLength] = useState(8)
